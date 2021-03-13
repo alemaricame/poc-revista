bc9a28d79d8c54c663f01b273473da96250eb780
ale <ale@tribuu.com>
2021-03-13 12:38:56 -0600

    fix(changes)

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,5 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+
+let headersReq = new HttpHeaders({
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Origin': '*'
+});
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +19,6 @@ export class UsuariosService {
   ) { }
 
   getAllUsers(){
-    return this.http.get('http://localhost:3000/api/users');
+    return this.http.get('https://poc-server-heroku.herokuapp.com/api/example', { headers: headersReq });
   }
 }
